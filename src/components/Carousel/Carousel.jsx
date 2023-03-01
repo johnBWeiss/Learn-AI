eacf710fd0ef002416b117cdcd8b3a9e8537facb
johnBWeiss <yoniw7@gmail.com>
2023-03-01 23:22:20 +0200

    passed page details

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -2,13 +2,28 @@ import Carousel from "react-multi-carousel";
 import { createSearchParams } from "react-router-dom";
 import "react-multi-carousel/lib/styles.css";
 import React from "react";
-import genericVector from "../../assets/images/back-2.jpg";
-import a from "../../assets/images/back-3.jpg";
-import b from "../../assets/images/back-4.jpg";
-import c from "../../assets/images/back-5.jpg";
 import "./Carousel.css";
 
 import { useNavigate } from "react-router";
+import { useState, useEffect } from "react";
+
+// const CarouselLib = ({ imgArray }) => {
+//   console.log(imgArray);
+//   const navigate = useNavigate();
+//   const handleClick = (src, path) => {
+//     navigate({
+//       pathname: "/image-details",
+//       search: createSearchParams({ id: src, path }).toString(),
+//     });
+//   };
+const shuffleArray = (array) => {
+  const newArray = [...array];
+  for (let i = newArray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+  }
+  return newArray;
+};
 
 const CarouselLib = ({ imgArray }) => {
   console.log(imgArray);
@@ -20,27 +35,31 @@ const CarouselLib = ({ imgArray }) => {
     });
   };
 
+  const [shuffledArray, setShuffledArray] = useState(imgArray);
+
+  useEffect(() => {
+    let shuffled = shuffleArray(imgArray);
+    setShuffledArray(shuffled);
+  }, [imgArray]);
+
   const responsive = {
     desktop: {
       breakpoint: { max: 6000, min: 1024 },
       items: 1,
-      //   slidesToSlide: 3, // optional, default to 1.
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
       items: 1,
-      //   slidesToSlide: 2, // optional, default to 1.
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
       items: 1,
-      //   slidesToSlide: 1, // optional, default to 1.
     },
   };
 
   return (
     <div className="CarouselLibContainer">
-      {imgArray && (
+      {shuffledArray && (
         <Carousel
           swipeable={true}
           draggable={true}
@@ -60,31 +79,16 @@ const CarouselLib = ({ imgArray }) => {
           dotListClass="custom-dot-list-style"
           itemClass="carousel-item-padding-40-px"
         >
-          {imgArray?.map((v, i) => (
-            <div
-              key={v.src}
-              // className="containerItem"
-              // style={{ ...jump }}
-            >
+          {shuffledArray?.map((v, i) => (
+            <div key={v.src}>
               <img
                 src={v.src}
                 alt="logo"
-                // height={imgSize}
-                //   width={"75vw"}
                 className="carouselImage"
-                // onClick={() => {
-                //   navigate("/", {
-                //     state: {
-                //       data: v.src,
-                //     },
-                //   });
-                // }}
                 onClick={() => {
                   handleClick(v.src, v.path);
                 }}
-                //   style={{ width:'65vw',objectFit: "cover",borderRadius:'30px',cursor:'pointer' }}
               />
-              {/* <div>{v.text}</div> */}
             </div>
           ))}
         </Carousel>
