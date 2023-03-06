aff873e3aef931c163ac64ece181de14342db431
johnBWeiss <yoniw7@gmail.com>
2023-03-06 22:57:47 +0200

    responsive

--- src/pages/Enlarged_Image.js
+++ src/pages/Enlarged_Image.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { useNavigate } from 'react-router'
 import { useSearchParams } from 'react-router-dom';
 import X from "../assets/x.png"
@@ -7,14 +7,18 @@ const Enlarged_Image = ({ location }) => {
     const [data] = useSearchParams()
     let src = (data.get("id"));
 
-
+  
+    useEffect(() => {
+        window.scrollTo({ top: 0, left: 0, behavior: "smooth" })
+     
+    }, [])
 
     const navigate = useNavigate()
     return (
-        <div style={{ backgroundImage: "url('../assets/images/back-4.jpg')", height: '100vh', width: '100vw', position: 'fixed', backgroundColor: 'rgb(0,0,0,0.45)', cursor: 'pointer', borderRadius: '20px', flexDirection: 'column' }} className='true-center ' > <img style={{ objectFit: 'contain' }} src={src} height='90%' width='90%' alt='img' />     <img
+        <div style={{ backgroundImage: "url('../assets/images/back-4.jpg')",marginTop:'', height: '90vh', width: '100vw', position: 'fixed', backgroundColor: 'rgb(0,0,0,0.45)', cursor: 'pointer', borderRadius: '20px', flexDirection: 'column' }} className='true-center ' > <img style={{ objectFit: 'contain' }} src={src} height='90%' width='90%' alt='img' />     <img
             src={X}
             className="topScroll "
-            style={{transform:'translateY(-30px)'}}
+            style={{transform:'translateY(0px)'}}
             alt="scroll to top"
             title="back"
             onClick={() =>
