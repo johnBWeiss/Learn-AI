d253412387357b983258ee60c45433a94e7aed4f
johnBWeiss <yoniw7@gmail.com>
2023-03-06 22:30:50 +0200

    responsive

--- src/utils/functions.js
+++
@@ -1,8 +0,0 @@
-
-// the shorten function recives texts and slices them according to dynamic values.
-
-export const shorten = (text, limit) => {
-    let slicedText = text?.substring(0, limit) ?? ''
-    slicedText = text?.length >= limit ? slicedText + '...' : slicedText
-    return slicedText
-}
